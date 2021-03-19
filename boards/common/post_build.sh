3caf7f3704590eb076e2f84ba2ecd13414c63a98
Dolu1990 <charles.papon.90@gmail.com>
2021-03-19 09:11:31 +0100

    renice haveged

--- boards/common/post_build.sh
+++ boards/common/post_build.sh
@@ -2,3 +2,5 @@
 LINUX_ADDRESS=0x80000000
 $HOST_DIR/bin/mkimage -A riscv -O linux -T kernel -C none -a $LINUX_ADDRESS -e $LINUX_ADDRESS -n Linux -d $BINARIES_DIR/Image $BINARIES_DIR/uImage
 
+# haveged : change priority
+sed -i 's/start-stop-daemon -S -x/start-stop-daemon -S -N 10 -x/' $TARGET_DIR/etc/init.d/S21haveged
