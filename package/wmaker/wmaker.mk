0783153ce99ca6551218b420cc7a834bb52bc5d5
Dolu1990 <charles.papon.90@gmail.com>
2021-05-15 01:34:58 +0200

    Fix wmaker

--- package/wmaker/wmaker.mk
+++ package/wmaker/wmaker.mk
@@ -8,31 +8,9 @@ WMAKER_SITE = https://github.com/window-maker/wmaker.git
 WMAKER_VERSION = wmaker-0.95.9
 WMAKER_SITE_METHOD = git
 WMAKER_GIT_SUBMODULES = YES
-WMAKER_INSTALL_STAGING_OPTS	= install
-WMAKER_INSTALL_TARGET_OPTS	= install
-WMAKER_CONF_OPTS =  --with-incs-from="" \
-                    --with-libs-from=""  \
-                    --oldincludedir=$(TARGET_DIR)/usr/include \
-                    --includedir=$(TARGET_DIR)/usr/include \
-	                --x-includes=$(STAGING_DIR)/usr/include \
-	                --x-libraries=$(STAGING_DIR)/usr/lib \
-	                --libdir=$(TARGET_DIR)/usr/lib \
-                    --bindir=$(TARGET_DIR)/bin \
-                    --sbindir=$(TARGET_DIR)/sbin \
-                    --libexecdir=$(TARGET_DIR)/libexec \
-                    --sysconfdir=$(TARGET_DIR)/etc \
-                    --sharedstatedir=$(TARGETG_DIR)/com \
-                    --localstatedir=$(TARGET_DIR)/var      \
-                    --datarootdir=$(TARGET_DIR)/share \
-                    --datadir=$(TARGET_DIR) \
-                    --infodir=$(TARGET_DIR)/info \
-                    --localedir=$(TARGET_DIR)/locale \
-                    --mandir=$(TARGET_DIR)/man \
-                    --docdir=$(TARGET_DIR)/doc/WindowMaker \
-                    --htmldir=$(TARGET_DIR) \
-                    --dvidir=$(TARGET_DIR) \
-                    --pdfdir=$(TARGET_DIR) \
-                    --psdir=$(TARGET_DIR)
+WMAKER_AUTORECONF = YES
+WMAKER_CONF_OPTS =
+
 
 WMAKER_DEPENDENCIES = xlib_libX11 xlib_libXft xlib_libXmu host-pkgconf
 
