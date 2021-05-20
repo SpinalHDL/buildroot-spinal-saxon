81dc030182c9906877194e8e9824bc7c491ac80d
Dolu1990 <charles.papon.90@gmail.com>
2021-05-21 01:28:03 +0200

    openttd fix lzma

--- package/openttd/openttd.mk
+++ package/openttd/openttd.mk
@@ -11,7 +11,7 @@ OPENTTD_SITE_METHOD = git
 OPENTTD_GIT_SUBMODULES = YES
 OPENTTD_CONF_OPTS =
 OPENTTD_SUPPORTS_IN_SOURCE_BUILD=NO
-OPENTTD_DEPENDENCIES = zlib lzma-target libpng sdl freetype fontconfig icu host-openttd host-pkgconf
+OPENTTD_DEPENDENCIES = xlib_libX11 sdl zlib xz libpng sdl freetype fontconfig icu host-openttd host-pkgconf
 HOST_OPENTTD_DEPENDENCIES =
 #OPENTTD_CXXFLAGS +=
 #OPENTTD_CONF_OPTS += -DCMAKE_CXX_FLAGS="$(TARGET_CXXFLAGS) $(OPENTTD_CXXFLAGS)"
