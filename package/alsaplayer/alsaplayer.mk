00f09cf7e48f8079df2783b3e3b96f6f07502c8b
Dolu1990 <charles.papon.90@gmail.com>
2021-05-28 17:35:09 +0200

    sync

--- package/alsaplayer/alsaplayer.mk
+++ package/alsaplayer/alsaplayer.mk
@@ -9,10 +9,10 @@ ALSAPLAYER_VERSION = 65967c212961ef13c24a06d28ba57c8a4da11de2
 ALSAPLAYER_SITE_METHOD = git
 ALSAPLAYER_GIT_SUBMODULES = YES
 ALSAPLAYER_AUTORECONF = NO
-ALSAPLAYER_CONF_OPTS = --enable-oggvorbis=no
+ALSAPLAYER_CONF_OPTS = --enable-oggvorbis=no -enable-oss=no
 
 
-ALSAPLAYER_DEPENDENCIES = host-pkgconf
+ALSAPLAYER_DEPENDENCIES = dbus-glib libmad alsa-lib host-pkgconf
 
 define ALSAPLAYER_RUN_AUTOGEN
 	cd $(@D) && PATH=$(BR_PATH) ./autogen.sh
